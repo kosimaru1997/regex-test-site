dcfec9428f6058f8653776ff00a6ba788f646659
kosimaru1997 <kosimaru1997@gmail.com>
2022-07-25 02:59:52 +0900

    ReactQuery devtoolsのインストール、 初期設定

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 0,
+      suspense: true,
+      useErrorBoundary: true,
+    },
+    mutations: {
+      retry: 0,
+      useErrorBoundary: true,
+    },
+  },
+});
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <QueryClientProvider client={queryClient}>
+    <React.StrictMode>
+      <App />
+      {process.env.NODE_ENV === 'development' && (
+        <ReactQueryDevtools initialIsOpen={false} />
+      )}
+    </React.StrictMode>
+    ,
+  </QueryClientProvider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
