ebb7596ceb24833ee306ab7809c954379364705e
kosimaru1997 <kosimaru1997@gmail.com>
2022-07-26 03:16:11 +0900

    ルーティングを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import { FC } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import { axiosInterceptor } from './axsiosInterceptor';
+import ProblemIndex from './routes/problem/ProblemIndex';
 import Top from './routes/Top';
 
-const App: FC = () => (
-  <Routes>
-    <Route path="/" element={<Top />} />
-  </Routes>
-);
+const App: FC = () => {
+  axiosInterceptor();
+
+  return (
+    <Routes>
+      <Route path="/" element={<Top />} />
+      <Route path="/problems" element={<ProblemIndex />} />
+    </Routes>
+  );
+};
 
 export default App;
