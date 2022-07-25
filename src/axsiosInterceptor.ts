95b0b0b99420d5252492e3bc72121c42ddb88ea6
kosimaru1997 <kosimaru1997@gmail.com>
2022-07-26 03:12:58 +0900

    axios設定を追加

---
+++ src/axsiosInterceptor.ts
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export const axiosInterceptor = () => {
+  // const accessToken = findAccessToken();
+
+  // Add a request interceptor
+  axios.interceptors.request.use(
+    (config) => {
+      const configDump = config;
+      configDump.baseURL = process.env.REACT_APP_SERVER_BASE_URL;
+      console.log(process.env.REACT_APP_SERVER_BASE_URL);
+      // configDump.headers = {
+      //   Authorization: `Bearer ${accessToken}`,
+      // };
+
+      return configDump;
+    },
+    (error) =>
+      // Do something with request error
+      Promise.reject(error),
+  );
+  // Add a response interceptor
+  axios.interceptors.response.use(
+    (response) =>
+      // Any status code that lie within the range of 2xx cause this function to trigger
+      // Do something with response data
+      response,
+    (error) =>
+      // Any status codes that falls outside the range of 2xx cause this function to trigger
+      // Do something with response error
+      Promise.reject(error),
+  );
+};
