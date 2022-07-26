3d60e55757488d47a9f144f8d0d91676a0038f9f
kosimaru1997 <kosimaru1997@gmail.com>
2022-07-27 00:23:39 +0900

    全問題取得処理の型修正,他微妙修正

--- src/components/atom/findAllProblems.ts
+++ src/components/atom/findAllProblems.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { ProblemType } from '../../model/problem';
 
-export const findAllProblems = async (): Promise<ProblemType[] | undefined> => {
+export const findAllProblems = async (): Promise<ProblemType[]> => {
   const response = await axios.get(`api/problems`, {});
 
   return (await response.data) as ProblemType[];
