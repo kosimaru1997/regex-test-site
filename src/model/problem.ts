16ba471c3d29d56f19cbdfb5a0e7bcfe2a7bd1e2
kosimaru1997 <kosimaru1997@gmail.com>
2022-08-01 22:01:14 +0900

    Problem関連の型定義を追加

--- src/model/problem.ts
+++ src/model/problem.ts
@@ -1,9 +1,22 @@
 export type ProblemType = {
-  id: number;
+  id: string;
   title: string;
   content: string;
   level: string;
-  test_case: string;
   created_at: Date;
   updated_at: Date;
+  problem_test_cases: TestCaseType[];
+};
+
+export type TestCaseType = {
+  id: string;
+  test_case: string;
+  is_collect: boolean;
+  created_at: Date;
+  updated_at: Date;
+};
+
+export type CheckTestCases = {
+  is_collect_regex: boolean;
+  checked_cases: TestCaseType[];
 };
